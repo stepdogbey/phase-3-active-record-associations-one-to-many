6b3e27c8e6f50a8dffa9a4ce304906dbd8d28e5c
stephen <stevedogbey@gmail.com>
2022-09-08 11:26:27 +0000

    code alone done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ActiveRecord::Base
+    has_many :reviews
+#     def reviews
+#     Review.where(game_id: self.id)
+#   end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
+    # def game
+    # # self is the review instance
+    # Game.find(self.game_id)
+    belongs_to :game
+#   end
   
 end
